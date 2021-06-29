dcf44fa91dccdd7d3ea3daa4e4a3eaf889629220
ronniejuan <hofmannronnie@gmail.com>
2021-06-29 15:48:30 +0200

    fetch eg1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,22 @@ import "babel-polyfill";
 
 // \/ All of your javascript should go here \/
 
+//POSTING DATA
+
+const user = {
+    name:"Juan",
+    email:"Juan@lobo.cl",
+    password:"****", 
+    message:"",
+    checkbox:"",
+}
+
+fetch("https://jsonplaceholder.typicode.com/users",{
+    method:"POST",
+    body:JSON.stringify(user)
+})
+
+.then(responsive =>responsive.json())
+.then(result=> console.log(result))
+
+
